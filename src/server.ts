e807333a5e855dcc7f0512953b5fce76df1163d2
Jesús Ramos <jesusramosdev@gmail.com>
2019-11-16 22:06:31 +0000

    Configurando nodemon

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,6 @@
 import express from 'express';
-import morgan from 'morgan';
 const app = express();
-app.use(morgan('combined'));
 
 app.use(express.json());
 
-app.listen(3000, () => console.log('Ready on on port 3000'));
+app.listen(3000, () => console.log('Ready on port 3000'));
